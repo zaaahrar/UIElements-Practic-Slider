ef58a6a1b1b6b1f2b5bc9b978833ca574a513fd2
zaaahrar <zaahrar@mail.ru>
2024-02-27 22:48:33 +0300

    finish

--- Assets/Scripts/Bar.cs
+++ Assets/Scripts/Bar.cs
@@ -6,6 +6,12 @@ public abstract class Bar : MonoBehaviour
     [SerializeField] protected Slider _slider;
     [SerializeField] protected Health _health;
 
+    private void Start()
+    {
+        _slider.maxValue = _health.MaxHealth;
+        _slider.value = _health.CurrentHealth;
+    }
+
     private void OnEnable()
     {
         _health.ChangingHealth += OnHealthChanged;
--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -2,12 +2,6 @@ using UnityEngine;
 
 public class HealthBar : Bar
 {
-    private void Start()
-    {
-        _slider.maxValue = _health.MaxHealth;
-        _slider.value = _health.CurrentHealth;
-    }
-
     public override void OnHealthChanged(float newHealth)
     {
         _slider.value = newHealth;
